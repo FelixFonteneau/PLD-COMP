205c74a46ad6ec47c26347161c77eb582c8fc64a
FelixFonteneau <felix.fonteneau@hotmail.com>
2020-02-25 14:21:37 +0100

    Affectation assez propre

--- compiler/visitor.h
+++ compiler/visitor.h
@@ -6,13 +6,16 @@
 
 #include "antlr4-runtime.h"
 #include "antlr4-generated/ifccBaseVisitor.h"
+#include <map>
 
-
+using namespace std;
 /**
  * This class provides an empty implementation of ifccVisitor, which can be
  * extended to create a visitor which only needs to handle a subset of the available methods.
  */
 class  Visitor : public ifccBaseVisitor {
+private:
+  unordered_map<string, int> integerVariablesTable; // <nom de la variable, adresse mémoire>
 public:
 
   virtual antlrcpp::Any visitAxiom(ifccParser::AxiomContext *ctx) override {
@@ -21,38 +24,39 @@ public:
 
   virtual antlrcpp::Any visitProg(ifccParser::ProgContext *ctx) override {
 
-     std::cout<<".text \n" "# declaration of ’text’ section (which means ’program’) # entry point to the ELF linker or loader."
+     cout<<".text \n" //"# declaration of ’text’ section (which means ’program’)" // entry point to the ELF linker or loader.
 		 		".global main \n"
 		 		"main: \n"
-		 		"# prologue \n"
-		 		"pushq %rbp"  "# save %rbp on the stack \n"
-		 		"movq %rsp, %rbp"  "# define %rbp for the current function \n"
-		 
-		 		"# body \n";
-	  
+ 		    "  # prologue\n"
+		 		"  pushq %rbp\n"  //save %rbp on the stack
+		 		"  movq %rsp, %rbp\n"  //define %rbp for the current function
+
+		 		"\n  # body \n";
+
 	  visitChildren(ctx);
-	  
-	  std::cout<<
-		 
-		 		"# epilogue \n"
-		 		"popq %rbp"  "# restore %rbp from the stack \n"
-		 		"ret"		"# return to the caller (here the shell) \n";
+
+	  cout<<
+ 	      "\n  # epilogue\n"
+		 		"  popq %rbp\n"  //restore %rbp from the stack
+		 		"  ret\n";	 //return to the caller (here the shell)
 
      return 0;
   }
 
   virtual antlrcpp::Any visitAff(ifccParser::AffContext *ctx) override {
-     int retval = stoi(ctx->CONST()->getText());
-     std::cout<<"movl $"<<retval<<", -4(%rbp)";
-	  
+    int retval = stoi(ctx->CONST()->getText());
+    string varialbeName = ctx->VAR()->getText();
+    int memoryAdress = 4;
+    cout<<" movl $"<<retval<<", -" << memoryAdress << "(%rbp)" << endl;
+    integerVariablesTable[varialbeName] = memoryAdress;
 	  return 0;
 }
-	
+
   virtual antlrcpp::Any visitRet(ifccParser::RetContext *ctx) override {
-	  int retval = stoi(ctx->VAR()->getText());
-	  std::cout<<"movl -4(%rbp), %eax";
+    string variable = ctx->VAR()->getText();
+    unordered_map<string, int>:: iterator itr = integerVariablesTable.find(variable);
+	  cout<<"  movl -" << itr->second <<"(%rbp), %eax" << endl;
     return 0;
   }
 
 };
-
