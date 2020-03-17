4358055d4e62a47e26441a20dad1882805181820
Billuc <billuc@hotmail.fr>
2020-03-17 10:08:53 +0100

    AffVar working for now

--- compiler/test/test.s
+++ compiler/test/test.s
@@ -1,1 +1,17 @@
-Error on position 1:0
+.text 
+.global main 
+main: 
+  # prologue
+  pushq %rbp
+  movq %rsp, %rbp
+
+  # body 
+ movl $42, -1(%rbp)
+ movl $100, -5(%rbp)
+ movl -5(%rbp), %eax
+ movl %eax, -1(%rbp)
+  movl -1(%rbp), %eax
+
+  # epilogue
+  popq %rbp
+  ret
