8f4017f97c1843f2fdbba1d5030d5e2edab9e274
Billuc <billuc@hotmail.fr>
2020-03-24 10:41:46 +0100

    Test 12 not working :(

--- tests/pld-test-output/tests-Init-11_Ex-Mi-Parcours/input.c
+++
@@ -1,9 +0,0 @@
-#include <inttypes.h>
-int main()
-{
-	int a,b,c;
-	a=17;
-	b=42;
-	c = a*a + b*b +1;
-	return c;
-}
--- tests/pld-test-output/tests-Init-7-4_testAffExprConstDiv/input.c
+++
@@ -1,6 +0,0 @@
-#include <inttypes.h>
-int main() {
-	int a = 1;
-	a = 21/7/3;
-	return a;
-}
