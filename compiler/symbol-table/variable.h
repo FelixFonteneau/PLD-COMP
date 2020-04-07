e5408b5112e2e1d82feb392ed9834976af432e43
FelixFonteneau <felix.fonteneau@hotmail.com>
2020-04-07 13:58:23 +0200

    clean code and finish merging

--- compiler/symbol-table/variable.h
+++ compiler/symbol-table/variable.h
@@ -71,7 +71,7 @@ public:
     // Contrat :
     //
 
-    Variable (string name, Type type, int address) : name(name), type(type), address(address), defined(false){}
+    Variable (string name, Type type, string address) : name(name), type(type), address(address), defined(false){}
     // Mode d'emploi :
     //
     // Contrat :
@@ -87,7 +87,7 @@ protected:
 //----------------------------------------------------- Attributs protégés
   string name;
   Type type;
-  int address;
+  string address;
   bool defined;
 
 
