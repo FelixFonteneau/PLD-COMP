7c06a542199f52e84d424c29499938f3a22a91a8
wlid <thomas.jorda@insa-lyon.fr>
2020-04-07 12:22:31 +0200

    test d'integration

---
+++ tests/tests/Init/20-FINAL/20-01_Final_testFinal.c
@@ -0,0 +1,51 @@
+int a,b;
+int c;
+int p = 98;
+
+int main(){
+	int a;
+	int b;
+	int c;
+	int d = 36;
+	char e;
+	char f = 't';
+	char g = 45;
+	a = 12;
+	b = 47;
+	c = d;
+	e = f;
+	c = g;
+	d = f;
+	while ( a < 15 ){
+		if (b > c){
+			b = getNumberOne();
+			b = p;
+		}
+		else if ( b == c){
+			b = returnParamValue(75);
+		}
+		else {
+			b = c;
+		}
+		a = a + 1;
+	}
+	f = ((((a+b)*8)-(c-2)*d)+((9-6)*e)+(9+1*2))-p;
+	return f;
+}
+
+int getNumberOne(){
+	int h,i,j;
+	char k,l,m;
+	m = 77;
+	p = 1;
+	return m;
+}
+
+
+int returnParamValue(int value){
+	int h = 53;
+	int l = 122;
+	char i = 'y';
+	p = 44;
+	return value;
+}
